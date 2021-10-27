5f9260930a31e8cedc85a476b94851994849a3e0
Suellen Castro <suellen.cpires@outlook.com>
2021-10-27 15:19:18 -0300

    Iniciando projeto

---
+++ src/main/java/DesafioBomtrato/ModuloAprovacoes/ModuloAprovacoesApplication.java
@@ -0,0 +1,13 @@
+package DesafioBomtrato.ModuloAprovacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloAprovacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloAprovacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DesafioBomtrato/ModuloAprovacoes/ModuloAprovacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioBomtrato.ModuloAprovacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloAprovacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
